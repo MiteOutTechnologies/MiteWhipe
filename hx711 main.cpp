994f3c2d804cb32b8943816cb91a3203002a3a28
MiteOut Technologies <miteouttech@gmail.com>
2025-06-13 22:59:07 -0400

    Update hx711 main.cpp

--- hx711 main.cpp
+++ hx711 main.cpp
@@ -12,6 +12,17 @@ int zero = 0;
 int sum = 0;
 HX711 scale;
 
+//SD Card
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+int sck = 14;
+int miso = 12;
+int mosi = 13;
+int cs = 15;
+String logMsg;
+String dataPacket;
+
 void setup() {
   Serial.begin(9600);
 
@@ -19,11 +30,16 @@ void setup() {
   scale.begin(SDA, SCL);
   scale.set_gain(128);
   Serial.println(getZero());
+
+  //SD Card
+  SPI.begin(sck, miso, mosi, cs); // Initialize SPI with fixed pins
+
 }
 
 void loop() {
   Serial.println(getWeight());
-  delay(10); 
+  delay(10);
+  dataPacket = zero + "/" + getWeight() + "/";
 }
 
 int getZero() {
@@ -38,3 +54,18 @@ int getZero() {
 float getWeight() {
   return (scale.read() - zero) / scaleFactor;
 }
+
+void logData(); {
+  while (!SD.begin(cs)) {}
+  File f = SD.open("/MiteOut_Data.txt", FILE_WRITE);
+  if (f){
+    f.println(dataPacket);
+    f.close();
+      logMsg = "File written: /MiteOut_Data.txt\n";
+      Serial.println(logMsg);
+    }
+    else{
+      logMsg = "Failed to write file\n";
+      Serial.println(logMsg);
+    }
+  }
